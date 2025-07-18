c6f7749374dda8e652dc3fd6f34b36bccb9b9e33
pongpoti <pong.poti@gmail.com>
2025-07-18 12:23:31 +0700

    c

---
+++ main.js
@@ -0,0 +1,9 @@
+import express from "express";
+import process from "node:process";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("listening");
+});
